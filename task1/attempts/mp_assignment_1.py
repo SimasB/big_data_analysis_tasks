8007b74517d247768e1acf81a076b43cff8eaa4e
SimasB <simasbijeikis@gmail.com>
2022-03-01 19:46:11 +0200

    .

--- task1/attempts/mp_assignment_1.py
+++ task1/attempts/mp_assignment_1.py
@@ -1,9 +1,7 @@
 """
-Big Data Analysis - Assignment 1
+Big Data Analysis
     Group 7 - Version 1
-        Gabriele Ruminaviciute
-        Goncalo Marques
-        Simas Bijeikis 
+    Assignment 1
 """
 
 import pandas as pd
@@ -11,6 +9,9 @@ import multiprocessing as mp
 import collections
 import time
 import matplotlib.pyplot as plt
+import random
+
+random.seed(0)
 
 # Please, calculate how many words are unique and count each word in the data set.
 # The separation of the word in a sentence should be done by a space character.
@@ -20,17 +21,16 @@ def count_words(abst):
     abst = abst.lower() # lowercasing all
     words = abst.split(" ") # spliting all
     counter = collections.Counter(words)
-
     return counter
 
 if __name__ == '__main__':
     print('Program Started!')
-    filename = "covid_abstracts.csv"
-    chunksize = 2000
+    filename = "covid_abstracts_test.csv"
+    chunksize = 1000
     
     # Number of cpus
     cpus = mp.cpu_count()
-    print(f"{cpus} cpus available")
+    print(f"{cpus} available")
     
     # Plot
     plot_cpus = range(1, cpus+1)
@@ -50,30 +50,24 @@ if __name__ == '__main__':
         c = 1
         # Process each chunk individualy. It can be processes in parallel as well.
         for chunk_df in iter_csv:
-            #print(f"Chunk being processed {c}, data index read: {c * chunksize}")
             c += 1
             
-            # Concatenate all abtstracts in the chunck
+            # concatenating all abtstracts in the chunck
             absts = ""
             for i in range(chunk_df.shape[0]):
                 absts += list(chunk_df["abstract"])[i]
             absts = [absts]
             
-            # Get Results
+            # pool apply
             results = pool.apply(count_words, args=(absts))
-                
+            
             # Append results
             for word in results:
                 if word in count_all_words.keys():
                     count_all_words[word] += results[word]
                 else:
                     count_all_words[word] = results[word]
-            
-            # Limiting number of chunks for debuging
-            # if c >= 2:
-            #     break
-    
-        
+
         len_unique = len(count_all_words.keys())
         sorted_counter = {k: v for k, v in sorted(count_all_words.items(), key=lambda item: item[1], reverse = True) if k != ""}        
         top10 = {}
@@ -88,13 +82,14 @@ if __name__ == '__main__':
         running_period = time.time() - start 
         plot_time.append(running_period)
         
-        print(f"CPUs used: {cpu} Running Time: {running_period} seconds")
-        #print(f"  Unique words: {str(len_unique)}")
-        #print(f"  Top10: {str(top10)}")
+        print(f"CPUs used: {cpu} with {running_period} seconds")
+
+    print("Unique words: " + str(len_unique))
+    print("Top10: " + str(top10))
         
     plt.plot(plot_cpus, plot_time)
     plt.title("Running Time vs Number of CPUs")
-    plt.xlabel("CPUs")
     plt.ylabel("Time (s)")
+    plt.xlabel("CPUs")
     plt.savefig("plot.png")
     plt.show()
